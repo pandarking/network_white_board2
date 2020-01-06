c69e5916f2c18d9dcc7b139d1bc0d613e70127f8
Pandarking <570169891@qq.com>
2020-01-06 09:57:48 +0800

    server's basic function

--- Server.py
+++ Server.py
@@ -4,13 +4,29 @@
 # @Email   : 570169891@qq.com
 # @File    : Server.py
 # @Software: PyCharm
-print('hello')
-print('world')
-print('world')
-print('world')
+import socket
+import time
+import threading
 
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.network = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.network.bind((self.host, self.port))
+        self.network.listen(20)
 
+        print(f'sever listen at {self.port}')
 
+    def start(self):
+        while 1:
+            client_socket, client_address = self.network.accept()
+            print(f'client {client_address} connected')
+            time.sleep(0.1)
+            threading.Thread(target=self.wait_for_user_nickname, args=[client_socket])
+
+    def wait_for_user_nickname(self):
+        pass
 
 
 
