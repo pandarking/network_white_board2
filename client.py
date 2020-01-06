f63b93e78ba895f9e2f804992f941cb03765fbea
Pandarking <570169891@qq.com>
2020-01-06 10:53:54 +0800

    client's basic function

---
+++ client.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/6 10:00
+# @Author  : Panboshen
+# @Email   : 570169891@qq.com
+# @File    : client.py
+# @Software: PyCharm
+import socket
+from UserDialog import UserDialog
+
+class Connection:
+    def __init__(self):
+        pass
