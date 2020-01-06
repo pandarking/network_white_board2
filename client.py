eeb72990d9fad3a8dfbc99a6bad74869641b2dc6
Pandarking <570169891@qq.com>
2020-01-06 15:20:17 +0800

    add a function that check if user's nickname has been taken

--- client.py
+++ client.py
@@ -16,21 +16,31 @@ class Connection:
         self.host = UserDialog._Ip
         self.port = UserDialog._Port
 
-        self.socket = socket.socket()
-        self.socket.connect((self.host, self.port))
-        data = self.socket.recv(3).decode()
+        self.soc_ket = socket.socket()
+        self.soc_ket.connect((self.host, self.port))
+        data = self.soc_ket.recv(3).decode()
         print(data)
 
-        UserDialog.getUserNickname()
-        self.nickname = UserDialog._Nickname
-        self.socket.sendall((self.nickname.encode('utf-8')))
+        usernames = self.soc_ket.recv(1024).decode('utf-8')
+        userlist = usernames.split()
+
+        while 1:
+            UserDialog.getUserNickname()
+            self.nickname = UserDialog._Nickname
+            if self.nickname in userlist:
+                UserDialog.show_error_box('用户名已存在')
+            else:
+                break
+        self.soc_ket.sendall((self.nickname.encode('utf-8')))
 
     def start(self):
         while 1:
             time.sleep(0.1)
 
 
-
+if __name__ == '__main__':
+    con = Connection()
+    print('start')
 
 
 
