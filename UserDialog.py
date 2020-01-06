cff7b44870764c8847d6a2d7206c38d409663ed5
Pandarking <570169891@qq.com>
2020-01-06 10:54:37 +0800

    client window's basic function

---
+++ UserDialog.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/6 10:03
+# @Author  : Panboshen
+# @Email   : 570169891@qq.com
+# @File    : UserDialog.py
+# @Software: PyCharm
+
+from tkinter import *
+
+class UserDialog:
+    _Ip = ''
+    _Port = 0
+    _Nickname = ''
+
+    @classmethod
+    def getUserIp(cls):
+        def getUserIpAndPort():
+            cls._Ip = e1.get()
+            cls._Port = int(e2.get())
+            ClientWindow.destroy()
+
+        ClientWindow = Tk()
+        ClientWindow.title('White Board Client')
+        Label(ClientWindow, text='请输入IP').grid(row=0)
+        Label(ClientWindow, text='IP').grid(row=1)
+        Label(ClientWindow, text='PORT').grid(row=2)
+        e1 = Entry(ClientWindow).grid(row=1, column=1)
+        e2 = Entry(ClientWindow).grid(row=2, column=1)
+
+        button = Button(ClientWindow, text='ok', command=getUserIpAndPort).grid(row=3, column=1)
+
+        ClientWindow.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
