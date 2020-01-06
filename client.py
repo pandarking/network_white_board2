7adee9f7ceec02c66e9a0276141e0d61b112fb49
Pandarking <570169891@qq.com>
2020-01-06 14:05:45 +0800

    client basic function

--- client.py
+++ client.py
@@ -6,7 +6,56 @@
 # @Software: PyCharm
 import socket
 from UserDialog import UserDialog
+import socket
+import time
+
 
 class Connection:
     def __init__(self):
-        pass
+        UserDialog.getUserIp()
+        self.host = UserDialog._Ip
+        self.port = UserDialog._Port
+
+        self.socket = socket.socket()
+        self.socket.connect((self.host, self.port))
+        data = self.socket.recv(3).decode()
+        print(data)
+
+        UserDialog.getUserNickname()
+        self.nickname = UserDialog._Nickname
+        self.socket.sendall((self.nickname.encode('utf-8')))
+
+    def start(self):
+        while 1:
+            time.sleep(0.1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
